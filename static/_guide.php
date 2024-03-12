1f45daf630e5a478209817ee3f0482899ad2254f
stroligo <gabrielstroligo@gmail.com>
2024-03-12 23:27:33 +0000

    projeto iniciado

--- static/_guide.php
+++ static/_guide.php
@@ -1,310 +1,146 @@
-<?php 
-    $current_page = 'forms';
-    get_header();
-?>
+<!-- Pega o Header -->
+<?php $current_page = 'guide'; get_header(); ?>
 
 <main>
-	<!--   
-	wysiwyg-gray (default)	Gray
-	wysiwyg-slate	Slate
-	wysiwyg-zinc	Zinc
-	wysiwyg-neutral	Neutral
-	wysiwyg-stone 
-	-->
-	<!-- Tipologia -->
-	<section class="container p-6 prose prose-inovahc  mx-auto mt-8">
-	<h1> By default, Tailwind removes all of the default browser styling from</h1>
-	<p class="lead">
-		Until now, trying to style an article, document, or blog post with
-		Tailwind has been a tedious task that required a keen eye for
-		typography and a lot of complex custom CSS.
-	</p>
-	<p>
-		By default, Tailwind removes all of the default browser styling from
-		paragraphs, headings, lists and more. This ends up being really
-		useful for building application UIs because you spend less time
-		undoing user-agent styles, but when you <em>really are</em> just
-		trying to style some content that came from a rich-text editor in a
-		CMS or a markdown file, it can be surprising and unintuitive.
-	</p>
-	<h2> By default, Tailwind removes all of the default browser styling from</h2>
-	<p>
-		By default, Tailwind removes all of the default browser styling from
-		paragraphs, headings, lists and more. This ends up being really
-		useful for building application UIs because you spend less time
-		undoing user-agent styles, but when you <em>really are</em> just
-		trying to style some content that came from a rich-text editor in a
-		CMS or a markdown file, it can be surprising and unintuitive.
-	</p>
-	<figure>
-		<img
-		src="https://images.unsplash.com/photo-1556740758-90de374c12ad?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1000&amp;q=80"
-		alt=""
-		/>
-		<figcaption>
-		Contrary to popular belief, Lorem Ipsum is not simply random text.
-		It has roots in a piece of classical Latin literature from 45 BC,
-		making it over 2000 years old.
-		</figcaption>
-	</figure>
-	<p>
-		We get lots of complaints about it actually, with people regularly
-		asking us things like:
-	</p>
-	<blockquote>
-		<p class="text-inovahc-800">
-		Why is Tailwind removing the default styles on my
-		<code>h1</code> elements? How do I disable this? What do you mean
-		I lose all the other base styles too?
-		</p>
-	</blockquote>
-	<pre>
-		<code class="language-html">
-		For years parents have espoused the health benefits of eating garlic bread with cheese to their
-		children, with the food earning such an iconic status in our culture that kids will often dress
-		up as warm, cheesy loaf for Halloween.
-		
-		But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
-		springing up around the country.
+    <!--  Breadcrumbs - Voltar -->
+    <section>
+        <div class="container mx-auto p-6 flex gap-5 flex-col ">
+            <!--  Breadcrumbs - Voltar -->
+            <div>
+                <a href="home" class="flex gap-2">
+                    <button class="btn-icon btn-icon-small">
+                    <?php svg('icon-seta-esquerda',12,12,"fill-white");?> 
+                    </button>
+                    <button class="link">
+                        voltar
+                    </button>
+                </a>
+            </div>
+        </div>
+    </section>
+    <!-- Section Search  -->
+    <section>
+        <div class="container mx-auto p-6 flex flex-col md:flex-row md:gap-20 gap-10">
+            <!-- Post - Article -->
+            <article class="flex-1">
+                <figure>
+                    <img src="./assets/img/foto3.jpg" alt="Foto Modelo 1"/>
+                </figure>
+                <div class="post-header">
+                    <div class="post-header-categoria">Projeto</div>
+                    <div class="post-header-titulo">Projeto</div>
+                    <div class="post-header-info">08/09/2023</div>
+                </div>
+         
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+                <p>Para centros médicos e profissionais de saúde que tratam pacientes diabéticos, a BIOO associa conhecimento científico e tecnológico para mudar a realidade de pacientes com complicações do “pé diabético” e que poderiam ser evitadas em até 85% se atendidas precocemente. </p>
+                <figure>
+                    <img
+                    src="./assets/img/foto3.jpg"
+                    alt="Foto Modelo 1"
+                    />
+                </figure>
+                <p class="caption">
+                     Legenda da imagem
+                </p>
+                <h3>Subtítulo da notícia</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+                <blockquote>
+                    “Lorem ipsum dolor amet, consectetur adipiscing elit, sed do eiusmod reprehendit nostrud enum.”
+	            </blockquote>
+                <h3>Arquivos para download</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <p>
+            </article>
 
-		</code>
-	</pre>
-	<p>
-		We hear you, but we're not convinced that simply disabling our base
-		styles is what you really want. You don't want to have to remove
-		annoying margins every time you use a <code>p</code> element in a
-		piece of your dashboard UI. And I doubt you really want your blog
-		posts to use the user-agent styles either — you want them to look
-		<em>awesome</em>, not awful.
-	</p>
-	<p>
-		The <code>@tailwindcss/typography</code> plugin is our attempt to
-		give you what you <em>actually</em> want, without any of the
-		downsides of doing something stupid like disabling our base styles.
-	</p>
-	<p>
-		It adds a new <code>prose</code> class that you can slap on any
-		block of vanilla HTML content and turn it into a beautiful,
-		well-formatted document:
-	</p>
+             <!-- Footer somente mobile -->
+             <section class="flex flex-col md:hidden">
+                <!-- Temas relacionados -->
+                <div>
+                    <div class=" text-inovahc-green-800 text-3xl font-poppins mb-4">
+                        Temas relacionados
+                    </div>
+                    <div class="flex flex-wrap gap-4">
+                        <button class="tag tag-big">Tag A</button>
+                        <button class="tag tag-big">Tag B</button>
+                        <button class="tag tag-big">Tag C</button>
+                    </div>
+                </div>
+            </section>
 
-	<p>Hopefully that looks good enough to you.</p>
-	<h3>What about nested lists?</h3>
-	<p>
-		Nested lists basically always look bad which is why editors like
-		Medium don't even let you do it, but I guess since some of you
-		goofballs are going to do it we have to carry the burden of at least
-		making it work.
-	</p>
-	<ol>
-		<li>
-		<strong>Nested lists are rarely a good idea.</strong>
-		<ul>
-			<li>
-			You might feel like you are being really "organized" or
-			something but you are just creating a gross shape on the
-			screen that is hard to read.
-			</li>
-			<li>
-			Nested navigation in UIs is a bad idea too, keep things as
-			flat as possible.
-			</li>
-			<li>
-			Nesting tons of folders in your source code is also not
-			helpful.
-			</li>
-		</ul>
-		</li>
-		<li>
-		<strong>
-			Since we need to have more items, here's another one.
-		</strong>
-		<ul>
-			<li>
-			I'm not sure if we'll bother styling more than two levels
-			deep.
-			</li>
-			<li>
-			Two is already too much, three is guaranteed to be a bad idea.
-			</li>
-			<li>If you nest four levels deep you belong in prison.</li>
-		</ul>
-		</li>
-		<li>
-		<strong>
-			Two items isn't really a list, three is good though.
-		</strong>
-		<ul>
-			<li>
-			Again please don't nest lists if you want people to actually
-			read your content.
-			</li>
-			<li>Nobody wants to look at this.</li>
-			<li>I'm upset that we even have to bother styling this.</li>
-		</ul>
-		</li>
-	</ol>
-	<p>
-		The most annoying thing about lists in Markdown is that
-		<code>&lt;li&gt;</code> elements aren't given a child
-		<code>&lt;p&gt;</code> tag unless there are multiple paragraphs in
-		the list item. That means I have to worry about styling that
-		annoying situation too.
-	</p>
-	<ul>
-		<li>
-		<p><strong>For example, here's another nested list.</strong></p>
-		<p>But this time with a second paragraph.</p>
-		<ul>
-			<li>These list items won't have <code>&lt;p&gt;</code> tags</li>
-			<li>Because they are only one line each</li>
-		</ul>
-		</li>
-		<li>
-		<p>
-			<strong>
-			But in this second top-level list item, they will.
-			</strong>
-		</p>
-		<p>
-			This is especially annoying because of the spacing on this
-			paragraph.
-		</p>
-		<ul>
-			<li>
-			<p>
-				As you can see here, because I've added a second line, this
-				list item now has a
-				<code>&lt;p&gt;</code> tag.
-			</p>
-			<p>This is the second line I'm talking about by the way.</p>
-			</li>
-			<li>
-			<p>
-				Finally here's another list item so it's more like a list.
-			</p>
-			</li>
-		</ul>
-		</li>
-		<li>
-		<p>
-			A closing list item, but with no nested list, because why not?
-		</p>
-		</li>
-	</ul>
-	<p>And finally a sentence to close off this section.</p>
-	<h2>There are other elements we need to style</h2>
-	<p>
-		I almost forgot to mention links, like
-		<a href="https://tailwindcss.com">
-		this link to the Tailwind CSS website
-		</a>
-		. We almost made them blue but that's so yesterday, so we went with
-		dark gray, feels edgier.
-	</p>
-	<p>We even included table styles, check it out:</p>
-	<table>
-		<thead>
-		<tr>
-			<th>Wrestler</th>
-			<th>Origin</th>
-			<th>Finisher</th>
-		</tr>
-		</thead>
-		<tbody>
-		<tr>
-			<td>Bret "The Hitman" Hart</td>
-			<td>Calgary, AB</td>
-			<td>Sharpshooter</td>
-		</tr>
-		<tr>
-			<td>Stone Cold Steve Austin</td>
-			<td>Austin, TX</td>
-			<td>Stone Cold Stunner</td>
-		</tr>
-		<tr>
-			<td>Randy Savage</td>
-			<td>Sarasota, FL</td>
-			<td>Elbow Drop</td>
-		</tr>
-		<tr>
-			<td>Vader</td>
-			<td>Boulder, CO</td>
-			<td>Vader Bomb</td>
-		</tr>
-		<tr>
-			<td>Razor Ramon</td>
-			<td>Chuluota, FL</td>
-			<td>Razor's Edge</td>
-		</tr>
-		</tbody>
-	</table>
-	<p>
-		We also need to make sure inline code looks good, like if I wanted
-		to talk about
-		<code>&lt;span&gt;</code> elements or tell you the good news about
-		<code>@tailwindcss/typography</code>.
-	</p>
-	<h3>Sometimes I even use <code>code</code> in headings</h3>
-	<p>
-		Even though it's probably a bad idea, and historically I've had a
-		hard time making it look good. This
-		<em>"wrap the code blocks in backticks"</em> trick works pretty well
-		though really.
-	</p>
-	<p>
-		Another thing I've done in the past is put a <code>code</code> tag
-		inside of a link, like if I wanted to tell you about the
-		<a href="https://github.com/tailwindcss/docs">
-		<code>tailwindcss/docs</code>
-		</a>
-		repository. I don't love that there is an underline below the
-		backticks but it is absolutely not worth the madness it would
-		require to avoid it.
-	</p>
-	<h4>We haven't used an <code>h4</code> yet</h4>
-	<p>
-		But now we have. Please don't use <code>h5</code> or
-		<code>h6</code> in your content, Medium only supports two heading
-		levels for a reason, you animals. I honestly considered using a
-		<code>before</code> pseudo-element to scream at you if you use an
-		<code>h5</code> or <code>h6</code>.
-	</p>
-	<p>
-		We don't style them at all out of the box because
-		<code>h4</code> elements are already so small that they are the same
-		size as the body copy. What are we supposed to do with an
-		<code>h5</code>, make it <em>smaller</em> than the body copy? No
-		thanks.
-	</p>
-	<h3>We still need to think about stacked headings though.</h3>
-	<h4>
-		Let's make sure we don't screw that up with
-		<code>h4</code> elements, either.
-	</h4>
-	<p>
-		Phew, with any luck we have styled the headings above this text and
-		they look pretty good.
-	</p>
-	<p>
-		Let's add a closing paragraph here so things end with a decently
-		sized block of text. I can't explain why I want things to end that
-		way but I have to assume it's because I think things will look weird
-		or unbalanced if there is a heading too close to the end of the
-		document.
-	</p>
-	<p>
-		What I've written here is probably long enough, but adding this
-		final sentence can't hurt.
-	</p>
-
-	</section>
+            <!-- Sidebar somente Desktop -->
+            <aside class="md:w-[400px] hidden md:flex flex-col">
+                <!--  Lista de outros eventos, somente -->
+                <div class="flex flex-col">
+                    <figure class="md:w-32">
+                        <img src="./assets/img/sessao/eventos.svg" alt="Eventos" class="md:w-32">
+                    </figure>
+                    <div class=" text-inovahc-green-800 text-2xl md:text-3xl font-poppins mb-2">
+                        Confira outros eventos em destaque
+                    </div>
+                    <!--  Grid de eventos -->
+                    <div class=" grid grid-cols-1 py-8 gap-5">
+                        <!-- Card eventos -->
+                        <div class="flex flex-col md:flex-row gap-4">
+                            <figure class=" bg-inovahc-blue-800 rounded-lg md:w-1/3 max-h-[76px]">
+                                <img src="./assets/img/foto3.jpg" alt="evento" class="w-full max-h-[76px] rounded-lg object-cover">
+                            </figure>
+                            <div class="flex flex-col md:w-2/3">
+                                <div class="text-inovahc-purple-800 mb-1  font-semibold">Título lorem ipsum magna  consectetur nostrud tempor dolorem adipiscing</div>
+                                <div class="text-xs mb-2">09/2023</div>
+                                <div>
+                                    <button class="link">leia mais</button>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Card eventos -->
+                        <div class="flex flex-col md:flex-row gap-4">
+                            <figure class=" bg-inovahc-blue-800 rounded-lg md:w-1/3 max-h-[76px]">
+                                <img src="./assets/img/foto3.jpg" alt="evento" class="w-full max-h-[76px] rounded-lg object-cover">
+                            </figure>
+                            <div class="flex flex-col md:w-2/3">
+                                <div class="text-inovahc-purple-800 mb-1  font-semibold">Título lorem ipsum magna  consectetur nostrud tempor dolorem adipiscing</div>
+                                <div class="text-xs mb-2">09/2023</div>
+                                <div>
+                                    <button class="link">leia mais</button>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Card eventos -->
+                        <div class="flex flex-col md:flex-row gap-4">
+                            <figure class=" bg-inovahc-blue-800 rounded-lg md:w-1/3 max-h-[76px]">
+                                <img src="./assets/img/foto3.jpg" alt="evento" class="w-full max-h-[76px] rounded-lg object-cover">
+                            </figure>
+                            <div class="flex flex-col md:w-2/3">
+                                <div class="text-inovahc-purple-800 mb-1  font-semibold">Título lorem ipsum magna  consectetur nostrud tempor dolorem adipiscing</div>
+                                <div class="text-xs mb-2">09/2023</div>
+                                <div>
+                                    <button class="link">leia mais</button>
+                                </div>
+                            </div>
+                        </div>
+                    
+                    </div>
+                </div>
+                <!--  Temas relacionados -->
+                <div class="flex flex-col">
+                    <div class=" text-inovahc-green-800 text-3xl font-poppins mb-4">
+                        Temas relacionados
+                    </div>
+                    <div class="flex flex-wrap gap-4">
+                        <button class="tag tag-big">Tag A</button>
+                        <button class="tag tag-big">Tag B</button>
+                        <button class="tag tag-big">Tag C</button>
+                    </div>
+                </div>
+            </aside>
+        </div>
+    </section>
 
 
 
+  
 </main>
-
-
 <?php get_footer(); ?>
+
 
 
