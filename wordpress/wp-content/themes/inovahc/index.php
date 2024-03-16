d314a4af7e368137c010a052c67b761c6ca6d0e0
mariozenmedina <mazen.mario@gmail.com>
2024-03-16 06:47:50 -0300

    hero

--- wordpress/wp-content/themes/inovahc/index.php
+++ wordpress/wp-content/themes/inovahc/index.php
@@ -1,2 +1,11 @@
 <?php get_header(); ?>
+    <?php if( have_rows('conteudo_flexivel') ): while( have_rows('conteudo_flexivel') ): the_row(); ?>
+        <?php
+            switch(get_row_layout()){
+                case 'hero':
+                    include(get_stylesheet_directory() . '/partes/flexible_hero.php');
+                    break;
+            }
+        ?>
+    <?php endwhile; endif; ?>
 <?php get_footer(); ?>
