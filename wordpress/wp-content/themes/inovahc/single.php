1b37e67daf969879fc6c9d3f4824383ef0a82b60
stroligo <gabrielstroligo@gmail.com>
2024-03-17 18:21:24 +0000

    front finalizado

--- wordpress/wp-content/themes/inovahc/single.php
+++ wordpress/wp-content/themes/inovahc/single.php
@@ -36,6 +36,9 @@
                         <div class="post-header-info"><?php the_date('d/m/Y'); ?><?php $autoria = get_field('autoria'); if($autoria){ echo ' | '.$autoria; } ?></div>
                     </div>
 
+                    <p>TESTE GABRIEL </p>
+
+
                     <?php if( have_rows('conteudo_single') ): while( have_rows('conteudo_single') ): the_row();
                         switch(get_row_layout()){
                             case 'lide':
