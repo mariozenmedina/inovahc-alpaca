31475abb8d375559b80fcf1ed4e68f6b07df1bc9
mariozenmedina <mazen.mario@gmail.com>
2024-03-17 23:10:38 -0300

    darkmode favicon

--- wordpress/wp-content/themes/inovahc/header.php
+++ wordpress/wp-content/themes/inovahc/header.php
@@ -25,7 +25,8 @@
 <meta name="HandheldFriendly" content="True" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <!-- FIM DOS METAS -->
-<link rel="shortcut icon" href="<?php tu(); ?>/img/icon.png" />
+<link rel="shortcut icon" href="<?php tu(); ?>/img/favicon.png" media="(prefers-color-scheme: light)" />
+<link rel="shortcut icon" href="<?php tu(); ?>/img/favicon-darkmode.png" media="(prefers-color-scheme: dark)" />
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
