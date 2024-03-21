be3ae09e478f8c05af3cec657ea37ca57e6b9427
mariozenmedina <mazen.mario@gmail.com>
2024-03-21 10:37:03 -0300

    parallax reduzido

--- wordpress/wp-content/themes/inovahc/partes/flexible_decoy.php
+++ wordpress/wp-content/themes/inovahc/partes/flexible_decoy.php
@@ -6,8 +6,8 @@
                 <div class="figure-area">
                     <!-- Versao Desktop Parallax -->
                     <figure id="scene" ref="scene" class="md:flex hidden">
-                        <?php $galeria = get_sub_field('imagens'); foreach($galeria as $img): ?>
-                        <img data-depth="0.<?php echo mt_rand(1,5); ?>" src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>">
+                        <?php $galeria = get_sub_field('imagens'); foreach($galeria as $k => $img): ?>
+                        <img data-depth="<?php $rnd = mt_rand(-20,20)/100; echo $k ? ($rnd ? $rnd : .5) : 0; ?>" src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>">
                         <?php endforeach; ?>
                     </figure>
                     <!-- Versao Mobile sem Parallax -->
