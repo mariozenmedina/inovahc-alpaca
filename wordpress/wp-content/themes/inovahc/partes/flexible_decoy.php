121d9175dd580adbfdb16d22a73e8a45cf2760b1
mariozenmedina <mazen.mario@gmail.com>
2024-04-24 02:54:04 -0300

    multiplas seções parallax na mesma página

--- wordpress/wp-content/themes/inovahc/partes/flexible_decoy.php
+++ wordpress/wp-content/themes/inovahc/partes/flexible_decoy.php
@@ -5,15 +5,18 @@
             <div class="banner-container">
                 <div class="figure-area">
                     <!-- Versao Desktop Parallax -->
-                    <figure id="scene" ref="scene" class="md:flex hidden">
+                    <?php $uniq = uniqid(); ?>
+                    <figure id="scene<?php echo $uniq; ?>" ref="scene<?php echo $uniq; ?>" class="parascene md:flex hidden">
                         <?php $galeria = get_sub_field('imagens'); foreach($galeria as $k => $img): ?>
                         <img data-depth="<?php $rnd = mt_rand(-20,20)/100; echo $k ? ($rnd ? $rnd : .5) : 0; ?>" src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>">
                         <?php endforeach; ?>
                     </figure>
                     <!-- Versao Mobile sem Parallax -->
+                    <?php $img = get_sub_field('imagem'); if($img && isset($img['url']) && isset($img['alt'])): ?>
                     <figure class="flex md:hidden">
-                        <img src="<?php $img = get_sub_field('imagem'); echo $img['url']; ?>" alt="<?php echo $img['alt'] ?>" class="principal">
+                        <img src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt'] ?>" class="principal">
                     </figure>
+                    <?php endif; ?>
                 </div>
                 <div class="text-area"> 
                     <div class="banner-title"><?php the_sub_field('titulo'); ?></div>
